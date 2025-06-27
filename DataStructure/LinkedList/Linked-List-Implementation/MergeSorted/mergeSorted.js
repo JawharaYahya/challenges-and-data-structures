8a5a77022a13104f3008ce4792d28c8565ceaabf
JawharaYahya <jawharashhaltough199@gmail.com>
2025-06-28 00:27:55 +0300

    Add MergeSorted folder with README and source files

--- DataStructure/LinkedList/Linked-List-Implementation/MergeSorted/mergeSorted.js
+++ DataStructure/LinkedList/Linked-List-Implementation/MergeSorted/mergeSorted.js
@@ -1,13 +1,15 @@
  mergeSortedLists(list1,list2){
+
       const merged = new LinkedList;
       let current1 = list1;
          let current2 = list2;
+         
          while (current1 !== null && current2 !== null) {
             
             if (current1.data < current2.data) {
                merged.add(current1.data);
               current1= current1.next;
-            }
+            } 
             else {
              merged.add(current2.data);
              current2= current2.next;
